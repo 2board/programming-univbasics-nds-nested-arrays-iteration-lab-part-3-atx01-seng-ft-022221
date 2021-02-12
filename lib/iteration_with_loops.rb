1514385b6ecfbb8c6266a16fe35540924cb640d9
David Jun <djun301@gmail.com>
2021-02-12 18:42:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,6 +10,7 @@ while row_index < src.count do
   element_index += 1 
   end
 row_index += 1
+end
 p only_strings.join(" ")
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
